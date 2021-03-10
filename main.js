653b4d0d26a7088a7816d457b4f32501bd920fdf
Aarav65 <76677576+Aarav65@users.noreply.github.com>
2021-03-10 12:59:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+canvas = document.getElementById("my_olympic");
+ctx = canvas.getContext('2d');
+
+ctx.beginPath();
+ctx.strokeStyle= "grey";
+ctx.lineWidth = 4;
+ctx.rect(180, 143, 430, 250, 200);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "blue";
+ctx.lineWidth = 5;
+ctx.arc(290, 210, 40, 0, 2*Math.PI)
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "black";
+ctx.lineWidth = 5;
+ctx.arc(390, 210, 40, 0, 2*Math.PI)
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "red";
+ctx.lineWidth = 5;
+ctx.arc(490, 210, 40, 0, 2*Math.PI)
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "yellow";
+ctx.lineWidth = 5;
+ctx.arc(340, 250, 40, 0, 2*Math.PI)
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "green";
+ctx.lineWidth = 5;
+ctx.arc(440, 250, 40, 0, 2*Math.PI)
+ctx.stroke();
